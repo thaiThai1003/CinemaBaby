bd68067993dbf5ac8ba8eaceee7fbb903fd37d99
Thái Trinhj Duy <trinhduythai13@gmail.com>
2026-03-29 23:42:26 +0700

    Add booking flow, seat selection, payment QR and update database

--- CinemaBaby/Controllers/QRController.cs
+++ CinemaBaby/Controllers/QRController.cs
@@ -1,36 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QRCoder;
-using System.Drawing;
-using System.IO;
 
 namespace CinemaBaby.Controllers
 {
     public class QRController : Controller
     {
-        public IActionResult Index()
+        public IActionResult GenerateQR(decimal amount)
         {
-            return View();
-        }
-
-        public IActionResult GenerateQR()
-        {
-            string qrContent = @"MB Bank
+            string qrContent = $@"MB Bank
 STK: 123456789
 Tên: CINEMA BABY
-Số tiền: 100000
-Nội dung: DEMO_QR";
+Số tiền: {amount}
+Nội dung: THANH_TOAN";
 
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrContent, QRCodeGenerator.ECCLevel.Q);
-            QRCode qrCode = new QRCode(qrCodeData);
 
-            Bitmap qrImage = qrCode.GetGraphic(20);
+            // 🔥 KHÔNG dùng Bitmap nữa
+            var qrCode = new PngByteQRCode(qrCodeData);
+            byte[] qrBytes = qrCode.GetGraphic(20);
 
-            using (MemoryStream ms = new MemoryStream())
-            {
-                qrImage.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-                return File(ms.ToArray(), "image/png");
-            }
+            return File(qrBytes, "image/png");
         }
     }
 }
